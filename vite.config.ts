f50ecdf319ab718d2c72cba4c63005a4ff635b5f
vendarixbr <contatovendari@gmail.com>
2026-06-08 22:19:29 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
   plugins: [
+    cloudflare(),
     tanstackStart(),
     tailwindcss(),
     tsConfigPaths(),
