1b89dea21a3303dd3fe170e73bfab2dcfb99b3f6
vendarixbr <contatovendari@gmail.com>
2026-06-08 21:13:50 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({ autoCodeSplitting: true }),
-    react(),
+    tanstackStart(),
     tailwindcss(),
     tsConfigPaths(),
   ],
